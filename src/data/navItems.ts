20fa59999b8e7e224d201afa3121d5fe911a129d
jmonu <jmonuofficial@gmail.com>
2025-04-23 20:05:45 +0530

    Create navItems.ts

---
+++ src/data/navItems.ts
@@ -0,0 +1,11 @@
+export const navItems = [
+  { name: 'Home', path: '/' },
+  { name: 'About', path: '/about' },
+  { name: 'Academics', path: '/academics' },
+  { name: 'Admissions', path: '/admissions' },
+  { name: 'Departments', path: '/departments' },
+  { name: 'Research', path: '/research' },
+  { name: 'Placements', path: '/placements' },
+  { name: 'Facilities', path: '/facilities' },
+  { name: 'Contact', path: '/contact' },
+];
