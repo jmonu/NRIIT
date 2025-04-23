14134604c64001578cda2d93c63b315d8d7bfbb9
jmonu <jmonuofficial@gmail.com>
2025-04-23 19:47:11 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1e3a8a',
+        secondary: '#3b82f6',
+        accent: '#facc15',
+      },
+    },
+  },
+  plugins: [],
+}
