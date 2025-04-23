1b1f27ecb48faaacec784003fd231f491710685b
jmonu <jmonuofficial@gmail.com>
2025-04-23 20:07:30 +0530

    Create quickLinks.tsx

---
+++ src/data/quickLinks.tsx
@@ -0,0 +1,6 @@
+export const quickLinks = [
+  { name: 'Admissions', path: '/admissions' },
+  { name: 'Results', path: '/results' },
+  { name: 'Syllabus', path: '/syllabus' },
+  { name: 'Campus Tour', path: '/campus-tour' },
+];
