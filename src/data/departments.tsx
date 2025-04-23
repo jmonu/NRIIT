5618b2560ae41fa20c7832ebd81ff6eae1614f3a
jmonu <jmonuofficial@gmail.com>
2025-04-23 20:08:10 +0530

    Create departments.tsx

---
+++ src/data/departments.tsx
@@ -0,0 +1,29 @@
+import { FaLaptopCode, FaMicrochip, FaPlug, FaCog, FaBuilding } from 'react-icons/fa';
+
+export const departments = [
+  {
+    name: 'Computer Science Engineering',
+    description: 'Explore the world of computing and software development.',
+    icon: <FaLaptopCode />,
+  },
+  {
+    name: 'Electronics & Communication Engineering',
+    description: 'Dive into circuits, signals, and communication systems.',
+    icon: <FaMicrochip />,
+  },
+  {
+    name: 'Electrical & Electronics Engineering',
+    description:-benzene 'Master power systems and electrical technologies.',
+    icon: <FaPlug />,
+  },
+  {
+    name: 'Mechanical Engineering',
+    description: 'Design and build innovative machines and systems.',
+    icon: <FaCog />,
+  },
+  {
+    name: 'Civil Engineering',
+    description: 'Shape the future with infrastructure and construction.',
+    icon: <FaBuilding />,
+  },
+];
