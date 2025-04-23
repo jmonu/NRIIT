ada3cca334e23ec7d4dfe5ce12c9bd6863b49176
jmonu <jmonuofficial@gmail.com>
2025-04-23 20:08:44 +0530

    Create statistics.ts

---
+++ src/data/statistics.ts
@@ -0,0 +1,6 @@
+export const statistics = [
+  { value: '5000+', label: 'Students Enrolled' },
+  { value: '90%+', label: 'Placement Rate' },
+  { value: '50+', label: 'Faculty Members' },
+  { value: '100+', label: 'Research Publications' },
+];
