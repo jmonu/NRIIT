2756c41837dbbd6892a39b7f11122e7ea5dcb87c
jmonu <jmonuofficial@gmail.com>
2025-04-23 20:06:20 +0530

    Create sliderData.ts

---
+++ src/data/sliderData.ts
@@ -0,0 +1,38 @@
+export const sliderData = [
+  {
+    image: 'https://i.ibb.co/Z6PF1xHh/943fc24f2976.jpg',
+    caption: 'Welcome to NRI Institute of Technology',
+  },
+  {
+    image: 'https://i.ibb.co/s9HgR10z/b3e08640e44f.jpg',
+    caption: 'Excellence in Engineering Education',
+  },
+  {
+    image: 'https://i.ibb.co/Tx65QwNn/1e27abec6e44.jpg',
+    caption: 'Innovate, Inspire, Impact',
+  },
+  {
+    image: 'https://i.ibb.co/j9jWdW1h/b6e51744087e.jpg',
+    caption: 'State-of-the-Art Campus Facilities',
+  },
+  {
+    image: 'https://i.ibb.co/3YT3qSMZ/18ae84338089.jpg',
+    caption: 'Empowering Future Leaders',
+  },
+  {
+    image: 'https://i.ibb.co/qMxYhpqG/e4e346a8cbe5.jpg',
+    caption: 'Research and Innovation Hub',
+  },
+  {
+    image: 'https://i.ibb.co/Q3GDs1N8/d2e709d296ce.jpg',
+    caption: 'Strong Industry Connections',
+  },
+  {
+    image: 'https://i.ibb.co/R4cTnKyz/8668a8797c75.jpg',
+    caption: 'Vibrant Student Life',
+  },
+  {
+    image: 'https://i.ibb.co/LdpGgBcp/d65fba037fab.jpg',
+    caption: 'Holistic Development Programs',
+  },
+];
